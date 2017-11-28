3c6acea82d59a71533c61a4e1c023a03f842c512
User <user@Userde-Mac-mini.local>
2017-11-28 16:27:19 +0800

    added a test class

---
+++ CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by User on 2017/11/28.
+//  Copyright © 2017年 Code Coalitlon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by User on 2017/11/28.
+//  Copyright © 2017年 Code Coalitlon. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
